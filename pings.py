a825d24c9201c183c7327e63cf32a29262e25f19
blue-firer <66651330+blue-firer@users.noreply.github.com>
2020-12-21 21:00:38 -0400

    Add files via upload

--- pings.py
+++ pings.py
@@ -1,34 +1,43 @@
-#Autor julio
+#Autor poorboy
+#Modulos a importar
+from requests import get, exceptions
+from tabulate import tabulate
+import socket as s
+from pyfiglet import Figlet
+from art import *
 
+#Ascii art
+tprint("Bienvenido", font="random")
+
+#Decorador
 def decor(func):
     def wrap():
-        print("===============")
+        print("===================================")
         func()
-        print("===============")
+        print("===================================")
     return wrap
 
+#Codigo para averiguar la ip
 def print_text():
-    print("Chequer de ping")
+    print("Ingrese la pagina para analizar")
 
 decorated = decor(print_text)
 decorated()
 
-from requests import get, exceptions
-import socket as s
-
-print("Ingrese la pagina: ")
-host = input()
+host = input("")
 
 print("Esta es la direccion ip del sitio " + host)
+print("=========================================")
+print("")
 print(f'of {host} is {s.gethostbyname(host)}')
 
-
+#Codigo para encontrar el dns activo
 def check_internet_connection():
     try:
         print("Ingrese la pagina completa (http o https): ")
-        get(input(''),  timeout =10)
+        print("")
+        get(input(''),  timeout = 30)
         print('El servidor esta conectado')
     except exceptions.ConnectionError:
         print('El servidor no conectado')
-
 check_internet_connection()
